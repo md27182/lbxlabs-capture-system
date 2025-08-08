686d1844eee9451e8907d10a751e3ac9c0aa52f4
md27182 <md27182@gmail.com>
2025-08-07 20:34:44 -0700

    -

--- control.py
+++ control.py
@@ -7,8 +7,8 @@ import keyboard
 import clr
 
 # Add reference to the C# SDK DLL
-clr.AddReference("CameraSdkCs")
-clr.AddReference("ImageSdkCs")
+clr.AddReference(r"CameraSdkCs")
+clr.AddReference(r"ImageSdkCs")
 from P1.CameraSdk import Camera
 from P1.ImageSdk import *
 import System
@@ -55,27 +55,28 @@ def jogMode(controller, camera):
         if keyboard.is_pressed('q'):
             controller.sendCommand('J2-' + str(NOD_JOG_DISTANCE))
         if keyboard.is_pressed('c'):
-            camera.TriggerCapture()
-            image_file = camera.WaitForImage()
-            System.IO.File.WriteAllBytes("test_raw.iiq", image_file.Data.ToArray())
+            if camera is not None:
+                camera.TriggerCapture()
+                image_file = camera.WaitForImage()
+                System.IO.File.WriteAllBytes("test_raw.iiq", image_file.Data.ToArray())
 
-            raw_image = RawImage(image_file.Data.Pointer, image_file.Data.Length)
+                raw_image = RawImage(image_file.Data.Pointer, image_file.Data.Length)
 
-            metadata = MetaDataBase(raw_image)
-            success = metadata.CreateXmpNamespace("calibration", "http://www.lbxlabs.com/calibration/");
-            if(success):
-                success = metadata.CreateMetaObject("xmp:calibration:extrinsics").SetString("Values of the cameras extrinsic matrix")
+                metadata = MetaDataBase(raw_image)
+                success = metadata.CreateXmpNamespace("calibration", "http://www.lbxlabs.com/calibration/");
+                if(success):
+                    success = metadata.CreateMetaObject("xmp:calibration:extrinsics").SetString("Values of the cameras extrinsic matrix")
 
-            # decodeConfig = DecodeConfig.Defaults();
-            # decodedImage = decodeConfig.ApplyTo(raw_image);
-            # System.IO.File.WriteAllBytes("bitmapBayer.bin", decodedImage.Data);
+                # decodeConfig = DecodeConfig.Defaults();
+                # decodedImage = decodeConfig.ApplyTo(raw_image);
+                # System.IO.File.WriteAllBytes("bitmapBayer.bin", decodedImage.Data);
 
-            convertConfig = ConvertConfig()
-            convertConfig.SetOutputWidth(14204)
-            tiffConfig = TiffConfig()
-            tiffConfig.tileSize = TiffTileSize.tileSize512
-            bitmap = convertConfig.ApplyTo(raw_image)
-            raw_image.WriteAsTiff("test_tiff.tiff", bitmap, metadata, tiffConfig)
+                convertConfig = ConvertConfig()
+                convertConfig.SetOutputWidth(14204)
+                tiffConfig = TiffConfig()
+                tiffConfig.tileSize = TiffTileSize.tileSize512
+                bitmap = convertConfig.ApplyTo(raw_image)
+                raw_image.WriteAsTiff("test_tiff.tiff", bitmap, metadata, tiffConfig)
 
 
             # System.IO.File.WriteAllBytes("bitmapRGB.bin", bitmap.Data);
@@ -86,10 +87,17 @@ def jogMode(controller, camera):
 
 def main():
     controller = Microcontroller(serial.Serial('COM3', 115200))
+    camera_connected = False
 
-    camera = Camera.OpenUsbCamera()
-    camera.EnableImageReceiving(True)
-    camera.SetHostStorageCapacity(1000000) # value in MB
+    try:
+        camera = Camera.OpenUsbCamera()
+    except:
+        print("No camera connected")
+        camera = None
+    if camera is not None:
+        camera.EnableImageReceiving(True)
+        camera.SetHostStorageCapacity(1000000) # value in MB
+
     # for property_id in list(camera.GetAllPropertyIds()):
     #     print(camera.GetPropertySpec(property_id))
     # P1.ImageSDK.Initalize()
@@ -99,9 +107,11 @@ def main():
         if val == 'exit':
             break
         valarr = val.split()
-        if valarr[0] == 'j':
-            jogMode(controller, camera)
+        if valarr:
+            if valarr[0] == 'j':
+                jogMode(controller, camera)
     
+    # if camera is not None:
     # camera.Close()
 
 if __name__ == '__main__':
